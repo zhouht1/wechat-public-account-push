aa6ce9a58af304779df3bce8856e47d00dd371c4
zhouht1 <113774785+zhouht1@users.noreply.github.com>
2023-09-11 16:31:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0cTH5tI4Ybwx8bXEUZ449hBwWQs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'z_YuPiFM15zeH95JWBVR3JfhC9wiZAlxBhVYkt_iJeU',
+      useTemplateId: 'DFqOuk3zpzTjUx_bkyv4CId6jfn9uUMuhGXOy1K1CUI',
       festivals: [
         // 注意：此条配置日期为阴历日期
         {
@@ -50,7 +50,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0cTH5kDb74CTUXx7sttyPMEES3o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'z_YuPiFM15zeH95JWBVR3JfhC9wiZAlxBhVYkt_iJeU',
+      useTemplateId: 'DFqOuk3zpzTjUx_bkyv4CId6jfn9uUMuhGXOy1K1CUI',
       festivals: [
         // 注意：此条配置日期为阴历日期
         {
