85268226f7d34fe5bc67d099b2abc32a087f80db
zhouht1 <113774785+zhouht1@users.noreply.github.com>
2023-09-11 14:27:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0cTH5kDb74CTUXx7sttyPMEES3o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'fZAqleXGWElOXdeXgihHt6g_YtJx0VcvTiR3Da3H9AA',
+      useTemplateId: '8Z_tHSQPAMJiH_BTAGzqlGOzb1WB-86w3Npd6fX-GNY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-12',
       festivals: [
