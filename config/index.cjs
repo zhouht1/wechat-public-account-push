97af477fcb624e1c6c924b3eff1bfc8813a33e9b
zhouht1 <113774785+zhouht1@users.noreply.github.com>
2023-09-11 14:23:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	v_VZUxOOoVkuZKCDP7-GAV3XQoaMWAX3MPzxTUba8qk',
+  CALLBACK_TEMPLATE_ID: 'v_VZUxOOoVkuZKCDP7-GAV3XQoaMWAX3MPzxTUba8qk',
 
   CALLBACK_USERS: [
     {
