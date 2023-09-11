58fbd9fca2217150384fc34505dce071eefbb90f
zhouht1 <113774785+zhouht1@users.noreply.github.com>
2023-09-11 14:51:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '庆饱饱',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o0cTH5kDb74CTUXx7sttyPMEES3o',
+      id: 'o0cTH5tI4Ybwx8bXEUZ449hBwWQs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'PQPAPy5sWERNJs9j2Y2LA3NVFWO93T654QGbcc846g0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -70,13 +70,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'v_VZUxOOoVkuZKCDP7-GAV3XQoaMWAX3MPzxTUba8qk',
+  CALLBACK_TEMPLATE_ID: '2-t6lo4aKSxist_aM-iNcvrJeUat-aPB8J9DBldhtds',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PQPAPy5sWERNJs9j2Y2LA3NVFWO93T654QGbcc846g0',
+      id: 'o0cTH5kDb74CTUXx7sttyPMEES3o',
     }
   ],
 
